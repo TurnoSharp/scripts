921e8710720d3a7241f16dcba6255114834f4a7a
TurnoSharp <turnosharp@gmail.com>
2022-01-18 18:39:44 +0000

    changed scripts for sending notifications to telegram

--- web_service_check.sh
+++ web_service_check.sh
@@ -12,29 +12,28 @@ BGRED='\033[41m'
 BGGREEN='\033[42m'
 BGBLUE='\033[44m'
 
+tg="~/.scripts/telegram.sh"
 #получаем статус web сервера
 nginxstatus=$(systemctl status nginx | grep -Eo "running|dead|failed")
 
 if [[ $nginxstatus = 'running' ]]
 then
-	echo -en "${WHITE} ${BGGREEN} Веб сервер работает ${NORMAL}\n"
+	$tg "nginx работает" > /dev/null
 else
-	echo -en "${WHITE} ${BGRED} nginx не работает ${NORMAL}\n"
+	$tg "nginx не работает" > /dev/null
 	systemctl restart nginx
 	sleep 1
-	echo -en "${WHITE} ${BGGREEN} Статус nginx после перезапуска $(systemctl status nginx | grep -Eo "running|dead|failed") ${NORMAL}\n"
-	echo $(curl -I 192.168.1.64 | grep ОК) 
+	$tg "Статус nginx после перезапуска $(systemctl status nginx | grep -Eo "running|dead|failed")!!" > /dev/null
 fi
 
 phpfpmstatus=$(systemctl status php7.2-fpm | grep -Eo "running|dead|failed")
 
 if [[ $phpfpmstatus = 'running' ]]
 then
-	echo -en "${WHITE} ${BGGREEN} PHP-FPM запущен ${NORMAL}\n"
+	$tg "PHP-FPM запущен" > /dev/null
 else
-	echo -en "${WHITE} ${BGRED} PHP-FPM не работает ${NORMAL}\n"
-	echo -en "Инициализирую перезапуск..."
+	$tg "PHP-FPM не работает" /dev/null
 	systemctl restart php7.2-fpm
 	sleep 1
-	echo -en "${WHITE} ${BGGREEN} Статус php-fpm после перезапуска $(systemctl status php7.2-fpm | grep -Eo "running|dead|failed") ${NORMAL}\n"
+	$tg "Статус php-fpm после перезапуска $(systemctl status php7.2-fpm | grep -Eo "running|dead|failed")!!" > /dev/null
 fi
